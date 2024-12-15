e161d8818e180186a89a6611092ef2df8e804bda
Beverly Green <greenbev000@gmail.com>
2024-12-15 15:48:56 -0600

    Rspec tests for vehicle factory passing

---
+++ spec/vehicle_factory_spec.rb
@@ -0,0 +1,31 @@
+require './lib/vehicle'
+require './lib/vehicle_factory'
+require './lib/dmv_data_service'
+require 'pry'
+RSpec.describe VehicleFactory do
+
+    before(:each) do
+        @factory = VehicleFactory.new
+        #vehicle = Vehicle.new({vin:'WBY8P8C51K', year: 2019, make: 'BMW', model: 'i3', engine: :ev} )
+    end
+
+    describe '#initialize' do
+        it 'checks Vehicle Factory class exists' do
+            expect(@factory).to be_a(VehicleFactory)
+        end
+    end
+
+    describe 'creates vehicles' do
+        it 'can create vehicles' do
+            wa_ev_registrations = DmvDataService.new.wa_ev_registrations
+            created_vehicles = @factory.create_vehicles(wa_ev_registrations)
+    
+            expect(@factory.created_vehicles[0].vin).to eq('WBY8P8C51K')
+            expect(@factory.created_vehicles[0].year).to eq('2019')
+            expect(@factory.created_vehicles[0].make).to eq('BMW')
+            expect(@factory.created_vehicles[0].model).to eq('i3')
+            expect(@factory.created_vehicles[0].engine).to eq(:ev)
+        end
+    end
+
+end
